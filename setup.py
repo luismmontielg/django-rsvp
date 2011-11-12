791fb60a3a07917e9699ab987e545f062e6a46e8
Luis Montiel <luismmontielg@gmail.com>
2011-11-12 16:35:30 -0600

    simple setup.py added

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-rsvp',
+    version='0.1.0',
+    description='A simple django RSVP app.',
+    long_description='',
+    keywords='django, rsvp',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='https://github.com/luismmontielg/django-rsvp',
+    license='',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['django',],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+)
